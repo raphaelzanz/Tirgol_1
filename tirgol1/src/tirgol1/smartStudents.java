0c1fa1b3b74b432a5cfa2ac2088e58bfa90068df
Student <Student@C0945-05.yosh.ac.il>
2016-02-29 17:41:39 +0200

    first try

---
+++ tirgol1/src/tirgol1/smartStudents.java
@@ -0,0 +1,11 @@
+package tirgol1;
+
+public class smartStudents {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+	}
+
+}
